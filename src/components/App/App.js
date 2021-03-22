ce6fdba5314fef530a5fd9e220b3e89df6bef099
Konstantin Meshkov <meshkovkm@giskaluga.ru>
2021-03-23 00:23:49 +0300

    feat: make logo 1280

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Header from "../Header/Header";
 
 function App() {
   return (
     <div className="page">
       <div className="page__container">
+        <Header />
         <h1>Test</h1>
       </div>
     </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import "./header.css";
 import logo from "../../images/logo.svg";
+import profile_icon from "../../images/profile.svg";
 
 function Header() {
   return (
@@ -8,6 +9,18 @@ function Header() {
       <a href="#" className="header_logo">
         <img className="logo logo_place_header" src={logo} alt="Mesto.Russia" />
       </a>
+      <div className="header_activity-container">
+        <nav>
+          <ul className="navigation">
+            <li className="navigation_link">Фильмы</li>
+            <li className="navigation_link">Сохранённые фильмы</li>
+          </ul>
+        </nav>
+        <div className="profile">
+          <p className="profile_name">Аккаунт</p>
+          <img className="profile_logo" src={profile_icon} alt="Профиль" />
+        </div>
+      </div>
     </header>
   );
 }
