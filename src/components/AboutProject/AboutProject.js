da642e1e7ee4dfa47dcc29d06b8d38e14c4d6895
Konstantin Meshkov <meshkovkm@giskaluga.ru>
2021-03-24 21:47:30 +0300

    feat: About in 1280 ready

---
+++ src/components/AboutProject/AboutProject.js
@@ -0,0 +1,37 @@
+import "./aboutProject.css";
+
+function AboutProject() {
+  return (
+    <section className="about">
+      <h1 className="about__title">О проекте</h1>
+      <div className="about__text-container">
+        <div>
+          <h2 className="text__title">Дипломный проект включал 5 этапов</h2>
+          <p className="text__article">
+            Составление плана, работу над бэкендом, вёрстку, добавление
+            функциональности и финальные доработки.
+          </p>
+        </div>
+        <div>
+          <h2 className="text__title">На выполнение диплома ушло 5 недель</h2>
+          <p className="text__article">
+            У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
+            соблюдать, чтобы успешно защититься.
+          </p>
+        </div>
+      </div>
+      <div className="time-bar">
+        <div className="time-bar__item time-bar__item_less">
+          <div className="bar bar_marked_green">1 неделя</div>
+          <p className="time-bar__text">Back-end</p>
+        </div>
+        <div className="time-bar__item time-bar__item_more">
+          <div className="bar bar_marked_grey">4 недели</div>
+          <p className="time-bar__text">Front-end</p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default AboutProject;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import Promo from "../Promo/Promo";
+import AboutProject from "../AboutProject/AboutProject";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="page__container">
         <Header />
         <Promo />
+        <AboutProject />
         <Footer />
       </div>
     </div>
