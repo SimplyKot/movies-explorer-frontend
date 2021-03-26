3e652696ea27f141bd3a844430223093470fe3c9
Konstantin Meshkov <meshkovkm@giskaluga.ru>
2021-03-26 14:05:50 +0300

    feat: prepare for Movies

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,14 @@
 import "./App.css";
 import Header from "../Header/Header";
+import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
-import Promo from "../Promo/Promo";
-import AboutProject from "../AboutProject/AboutProject";
-import Techs from "../Techs/Techs";
-import AboutMe from "../AboutMe/AboutMe";
-import Portfolio from "../Portfolio/Portfolio";
 
 function App() {
   return (
     <div className="page">
       <div className="page__container">
         <Header />
-        <Promo />
-        <AboutProject />
-        <Techs />
-        <AboutMe />
+        {/* <Main /> */}
         <Footer />
       </div>
     </div>
