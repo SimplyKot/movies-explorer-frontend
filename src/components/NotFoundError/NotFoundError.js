c664b4d40ead2da602d5de186778295eeddf4464
Konstantin Meshkov <meshkovkm@giskaluga.ru>
2021-03-30 23:38:49 +0300

    feat: 404 ready

--- src/components/NotFoundError/NotFoundError.js
+++ src/components/NotFoundError/NotFoundError.js
@@ -1,5 +1,19 @@
+import "./notfounderror.css";
 function NotFoundError() {
-  return <div> "NOT_FOUND"</div>;
+  return (
+    <div className="not-found">
+      <div className="not-found__container" />
+      <div className="not-found__container not-found__text-container">
+        <h1 className="not-found__title">404</h1>
+        <p className="not-found__message">Страница не найдена</p>
+      </div>
+      <div className="not-found__container not-found__link-container">
+        <a className="not-found__link" href="#">
+          Назад
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default NotFoundError;
