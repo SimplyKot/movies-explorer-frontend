bcf24dbd5d2ffbf69313aaf06a237852f120b4b8
Konstantin Meshkov <meshkovkm@giskaluga.ru>
2021-03-31 11:05:43 +0300

    feat: delete useless pictures

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,32 +1,32 @@
 import "./navigation.css";
-import { useHistory, withRouter, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import profile_icon from "../../images/profile.svg";
 
 function Navigation(props) {
   return (
     <ul className="navigation">
-      <li className="navigation_link__item">
+      <li className="navigation-link__item">
         <NavLink
-          className="navigation_link"
-          activeClassName="navigation_link_active"
+          className="navigation-link"
+          activeClassName="navigation-link_active"
           to="/movies"
         >
           Фильмы
         </NavLink>
       </li>
-      <li className="navigation_link__item">
+      <li className="navigation-link__item">
         <NavLink
-          className="navigation_link"
-          activeClassName="navigation_link_active"
+          className="navigation-link"
+          activeClassName="navigation-link_active"
           to="/saved-movies"
         >
           Сохранённые фильмы
         </NavLink>
       </li>
-      <li className="navigation_link__item">
+      <li className="navigation-link__item">
         <NavLink
-          className="navigation_link"
-          activeClassName="navigation_link_active"
+          className="navigation-link"
+          activeClassName="navigation-link_active"
           to="/profile"
         >
           <p className="profile__name">Аккаунт</p>
